12001bb311b82217a6836c381bef44c3b81cc69e
Emilio Dalla Torre <info@emiliodallatorre.it>
2021-10-20 14:04:37 +0200

    Alzata la qualità degli assets.

--- lib/assets.dart
+++ lib/assets.dart
@@ -44,11 +44,11 @@ class Assets {
 class Images {
   /// ![](file:///Users/emiliodallatorre/Documents/GitHub/jeve-orlertv-flutter/assets/icons/logo.png)
   static AssetImage get logo => const AssetImage("assets/icons/logo.png");
-  /// ![](file:///Users/emiliodallatorre/Documents/GitHub/jeve-orlertv-flutter/assets/images/lines.png)
-  static AssetImage get lines => const AssetImage("assets/images/lines.png");
-  /// ![](file:///Users/emiliodallatorre/Documents/GitHub/jeve-orlertv-flutter/assets/images/marble.png)
-  static AssetImage get marble => const AssetImage("assets/images/marble.png");
-  /// ![](file:///Users/emiliodallatorre/Documents/GitHub/jeve-orlertv-flutter/assets/images/splashWallpaper.png)
-  static AssetImage get splashWallpaper => const AssetImage("assets/images/splashWallpaper.png");
+  /// ![](file:///Users/emiliodallatorre/Documents/GitHub/jeve-orlertv-flutter/assets/images/lines.jpg)
+  static AssetImage get lines => const AssetImage("assets/images/lines.jpg");
+  /// ![](file:///Users/emiliodallatorre/Documents/GitHub/jeve-orlertv-flutter/assets/images/marble.jpg)
+  static AssetImage get marble => const AssetImage("assets/images/marble.jpg");
+  /// ![](file:///Users/emiliodallatorre/Documents/GitHub/jeve-orlertv-flutter/assets/images/splashWallpaper.jpg)
+  static AssetImage get splashWallpaper => const AssetImage("assets/images/splashWallpaper.jpg");
 }
 
